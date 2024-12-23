e01f102521edaf6de011a9a57b6d6aaa907d5787
HelyaTam <62566560+HelyaTam@users.noreply.github.com>
2024-12-23 14:46:08 +0100

    update fetch method

--- App/helpers/fetchPokemonApi.js
+++ App/helpers/fetchPokemonApi.js
@@ -7,6 +7,12 @@
 export async function fetchPokemonApi() {
   try {
     const response = await fetch("https://tyradex.vercel.app/api/v1/pokemon");
+
+    // Vérifie si la réponse du serveur est correcte (status 200-299)
+    if (!response.ok) {
+      throw new Error("La réponse du serveur n'est pas correcte.");
+    }
+
     const data = await response.json(); // Parse les données JSON de la réponse
 
     return data; // Retourne les Pokémon récupérés
